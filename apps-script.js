52280519633f26e7ff7102a9a071de1edf6a2ca3
DiscourseDigital.com <76527388+discoursedigital@users.noreply.github.com>
2021-10-06 11:10:52 -0400

    Added cahced function version

--- apps-script.js
+++ apps-script.js
@@ -38,3 +38,35 @@ function checkUrl(url){
   /return the value of status which will be an enum: True, False or null
   return status;
 }
+
+//This function calls and sets the cache for future calls if working with a large volume of links
+function checkUrlCache(url){
+  
+  let status = null;
+
+  const goodUrlCodes = [200,204,201,206];
+
+  //This is for caching responses to avoid hitting the daily quotas on refeshing the browser 
+  let cache = CacheService.getScriptCache();
+  let cached = cache.get(url);
+  if(cached && goodUrlCodes.find((elem) => elem = cached) === cached){
+    status = true;
+    return status;
+   }
+
+  try{
+
+   let responseCode = UrlFetchApp.fetch(url).getResponseCode();  
+   //Store the response in cache if the 
+   cache.put(url,responseCode, 21600);
+
+    if(responseCode && goodUrlCodes.find((elem) => elem = responseCode) === responseCode){
+      status = true;
+    }else{
+      status = false
+    }
+  }catch(err){ 
+    status = false;
+  }
+  return status;
+}
